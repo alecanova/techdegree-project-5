b03353ba6816b6e887b76502414d7fe938e266a0
Alessandro Canova <alessandrocanova14@gmail.com>
2020-07-19 12:41:11 +0200

    refactor fetch function

--- js/scripts.js
+++ js/scripts.js
@@ -3,52 +3,48 @@
 //------------------
 const galleryDiv = document.getElementById('gallery');
 const searchContainer = document.getElementsByClassName('search-container');
-const userURL = `https://randomuser.me/api/?results=12&nat=au,ca,gb,nl,nz,us`;
+//const userURL = `https://randomuser.me/api/?results=12&nat=au,ca,gb,nl,nz,us`;
 
 //------------------
 // Fetch function
 //------------------
-async function fetchUserData() {
+function fetchUserData(url) {
 
-    try {
+    return fetch(url)
 
-        const response = await fetch(userURL);
-        return await response.json();
-
-
-    } catch(error) {
-
-        throw error;
-
-    }
+        .then(response => response.json()) 
+        .then(data => data.results)
+        .catch(error => console.log('Looks like there was a problem', error))
   
 }
 
-//--------------
-// User Cards
-//--------------
-function generateUserHTML(data) {
+//-------------------
+// Generate User Card
+//-------------------
+function generateUserHTML(user, data) {
 
-    data.map(user => {
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'card';
+    galleryDiv.appendChild(cardDiv);
 
-        const cardDiv = document.createElement('div');
-        cardDiv.className = 'card';
-
-        const userHTML = `
+    const userHTML = `
         
-            <div class="card-img-container">
-                <img class="card-img" src=${user.picture.medium} alt="profile picture">
-            </div>
-            <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
-                <p class="card-text">${user.email}</p>
-                <p class="card-text cap">${user.location.city}, ${user.location.country}</p>
-            </div>
+        <div class="card-img-container">
+            <img class="card-img" src=${user.picture.medium} alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+            <p class="card-text">${user.email}</p>
+            <p class="card-text cap">${user.location.city}, ${user.location.country}</p>
+        </div>
 
-        `;
+    `;
 
-        cardDiv.innerHTML = userHTML;
-        galleryDiv.appendChild(cardDiv);
+    cardDiv.innerHTML = userHTML;
+
+    cardDiv.addEventListener('click', (e) => {
+
+        e.target = modalUserHTML(user, data);
 
     });
 
@@ -57,47 +53,54 @@ function generateUserHTML(data) {
 //----------------------
 // Generate Modal Window
 //----------------------
-function modalUserHTML(data) {
+function modalUserHTML(user) {
 
-    data.map(user => {
+    const modalDiv = document.createElement('div');
+    modalDiv.className = 'modal-container';
+    document.body.insertBefore( modalDiv, document.querySelector('script') );
 
-        const modalDiv = document.createElement('div');
-        modalDiv.className = 'modal-container';
+    const birthday = new Date(user.dob.date);
 
-        const birthday = new Date(user.dob.date);
+    const modalHTML = `
 
-        const modalHTML = `
-
-            <div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                <div class="modal-info-container">
-                    <img class="modal-img" src=${user.picture.large} alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
-                    <p class="modal-text">${user.email}</p>
-                    <p class="modal-text cap">${user.location.city}</p>
-                    <hr>
-                    <p class="modal-text">${user.cell}</p>
-                    <p class="modal-text">${user.location.street} ${user.location.city} ${user.location.state} 
-                    ${user.location.postcode} ${user.nat} </p>
-                    <p class="modal-text">${birthday.toLocaleDateString()}</p>
-                </div>
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src=${user.picture.large} alt="profile picture">
+                <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+                <p class="modal-text">${user.email}</p>
+                <p class="modal-text cap">${user.location.city}</p>
+                <hr>
+                <p class="modal-text">${user.cell}</p>
+                <p class="modal-text">${user.location.street.number} ${user.location.street.name} ${user.location.city}, 
+                ${user.location.state}, ${user.nat}, ${user.location.postcode}</p>
+                <p class="modal-text">Birthday: ${birthday.toLocaleDateString()}</p>
             </div>
+        </div>
 
             <div class="modal-btn-container">
-                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
-                </div>
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
             </div>
-    
-        `;
 
-        modalDiv.innerHTML = modalHTML;
-        document.body.insertBefore('modalDiv', document.querySelector('script') );
+    `;
 
-    })
+    modalDiv.innerHTML = modalHTML;
 
 }
 
+fetchUserData('https://randomuser.me/api/?results=12&nat=au,ca,gb,nl,nz,us')
+
+    .then( data => {
+
+        data.map(user => {
+
+            generateUserHTML(user, data)
+
+        })
+
+    })
+    
 
 
 
