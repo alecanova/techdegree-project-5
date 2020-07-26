48b36a8654dbb653204e04edf59779401df2a619
Alessandro Canova <alessandrocanova14@gmail.com>
2020-07-26 17:49:06 +0200

    refactor fenerateModalWindow function

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 // Global variables
 //------------------
 const gallery = document.getElementById('gallery');
+const userCardArr = [];
 
 //---------------
 // Fetch function
@@ -14,15 +15,17 @@ function fetchData(url) {
         .then(data => data.results )
         .catch(error => console.log('Looks like there was a problem', error))
 
-  }
+}
 
 //------------------------
 // Generate User Card HTML
 //------------------------
 
-function generateCardHTML(data) {
+const generateCard = (users) => {
 
-    const cards = data.map( user => 
+    users.map( user => {
+
+        const userCard = 
 
         `
         <div class="card">
@@ -36,22 +39,96 @@ function generateCardHTML(data) {
             </div>
         </div>
 
-        `
+        `;
 
-    );
+        userCardArr.push(userCard);
 
-        gallery.innerHTML = cards;
+    })
+
+    userCardArr.forEach(profile => {
+
+        gallery.innerHTML += profile;
+            
+    })
+
+        // Insert event click to open modal here
+    const cards = document.querySelectorAll('.card');
+
+        for (let i = 0; i < cards.length; i++) {
+        
+            cards[i].addEventListener('click', event => {
+
+                if (event.target === cards[i] || cards[i].contains(event.target) ) {
+
+                    generateModalWindow(users, i);
+
+                }
+
+            })
+
+         }
 
 }
 
+//------------------
+// Show Modal Window
+//------------------
+const generateModalWindow = (users, index) => {
+
+    const user = users[index];
+
+        const modalContainer = document.createElement('div');
+        modalContainer.classList.add('modal-container');
+
+        const birthday = new Date(user.dob.date);
+
+        modalContainer.innerHTML = `
+
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+                <p class="modal-text">${user.email}</p>
+                <p class="modal-text cap">${user.location.country}</p>
+                <hr>
+                <p class="modal-text">${user.cell}</p>
+                <p class="modal-text">${user.location.street.number} ${user.location.street.name}
+                ${user.location.city}, ${user.location.state}, ${user.nat}</p>
+                <p class="modal-text">Birthday: ${birthday.toLocaleDateString()}</p>
+            </div>
+        </div>
+        
+        `;
+
+        document.body.insertBefore(modalContainer, document.querySelector('script'));
+
+    const modalCloseBtn = document.getElementById('modal-close-btn');
+    modalContainer.addEventListener('click', event => {
+
+        if(event.target === modalCloseBtn || modalCloseBtn.contains(event.target)) {
+
+            modalContainer.remove();
+
+        }
+
+    })
+
+}
+
+
+
+
+//-----------
+// Fetch Data
+//-----------
 fetchData('https://randomuser.me/api/?results=12&nat=au,ca,gb,nl,nz,us')
 
     .then(data => {
 
-        data.results;
-        generateCardHTML(data);
-        
+        generateCard(data);
+
     })
 
-    
+
     
